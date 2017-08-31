4fa12d56ed4c91747af5899e415183d69a127e6d
Erick Lazon <elazon@uni.pe>
2017-08-31 16:53:25 -0500

    >:(

--- web2.py
+++ web2.py
@@ -9,11 +9,11 @@ webview=WebKit.WebView()
 headerbar=Gtk.HeaderBar()
 headerbar.set_show_close_button(True)
 go_back_button=Gtk.Button("Back")
+go_forward_button=Gtk.Button("Forward")
 #go.back.connect("clicked",lambda x: webview.go_back())
 #go_back_arrow=Gtk.Image.new_from_icon_name("go-previus",Gtk.IconSize.SMALL_TOOLBAR)
 #go_back_button.add(go_back_arrow)
 #No hay metodo on_go_back
-#go_back_button.connect("clicked",on_go_back)
 
 def on_enter(entry):
         url = entry.get_text()
@@ -29,12 +29,18 @@ def on_enter(entry):
         webview.open(url)
         
 def on_go_back(button):
-        Gtk.Button.go_back()
+        webview.go_back()
+
+def on_go_forward(button):
+        webview.go_forward()
 
 entry=Gtk.Entry()
 entry.connect("activate",on_enter)
+go_back_button.connect("clicked",on_go_back)
+go_back_button.connect("clicked",on_go_forward)
 headerbar.set_custom_title(entry)
 headerbar.pack_start(go_back_button)
+headerbar.pack_start(go_forward_button)
 
 
 scrolled_window=Gtk.ScrolledWindow()
