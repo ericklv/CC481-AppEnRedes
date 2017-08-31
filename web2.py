bbbe6289bbf6c8967104cef9c8294885572a309b
Erick Lazon <elazon@uni.pe>
2017-08-31 18:00:34 -0500

    buttons

--- web2.py
+++ web2.py
@@ -12,6 +12,7 @@ go_back_button=Gtk.Button("Back")
 go_forward_button=Gtk.Button("Forward")
 go_reload_button=Gtk.Button("Refresh")
 go_history_button=Gtk.Button("History")
+go_homepage_button=Gtk.Button("Home")
 #go.back.connect("clicked",lambda x: webview.go_back())
 #go_back_arrow=Gtk.Image.new_from_icon_name("go-previus",Gtk.IconSize.SMALL_TOOLBAR)
 #go_back_button.add(go_back_arrow)
@@ -32,27 +33,37 @@ def on_enter(entry):
         history_file.close()
         webview.open(url)
         
-def on_go_back(button):
+def on_go_back(widget,data=None):
         webview.go_back()
 
-def on_go_forward(button):
+def on_go_forward(widget,data=None):
         webview.go_forward()
 
-def on_go_reload(button):
-        webview.go_reload()
+def on_go_reload(widget,data=None):
+        webview.reload()
 
-def on_go_history(button):
+def on_go_history(widget,data=None):
         webview.open("/tmp/history.html")
+
+def go_homepage(widget, data=None):
+    page_="http://www.google.com"
+    webview.load_uri(page_)
 
 entry=Gtk.Entry()
 entry.connect("activate",on_enter)
 go_back_button.connect("clicked",on_go_back)
-go_back_button.connect("clicked",on_go_forward)
-headerbar.set_custom_title(entry)
+go_forward_button.connect("clicked",on_go_forward)
+go_reload_button.connect("clicked",on_go_reload)
+go_history_button.connect("clicked",on_go_history)
+go_homepage_button.connect("clicked",go_homepage)
+#headerbar.set_custom_title(entry)
+
 headerbar.pack_start(go_back_button)
 headerbar.pack_start(go_forward_button)
 headerbar.pack_start(go_reload_button)
+headerbar.pack_start(entry)
 headerbar.pack_start(go_history_button)
+headerbar.pack_start(go_homepage_button)
 
 scrolled_window=Gtk.ScrolledWindow()
 
