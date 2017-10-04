e223a0100adf1e974b0f1954013edca6e8b43b76
Erick Lazon <elazon@uni.pe>
2017-10-04 18:56:26 -0500

    middleware with seneca

---
+++ Node/seneca/index3.js
@@ -0,0 +1,32 @@
+const seneca = require('seneca')()
+const express = require('express')()
+const web = require('seneca-web')
+const cors = require('cors')
+
+var Routes = [{
+  prefix: '/products',
+  pin: 'area:product,action:*',
+  map: {list: {GET: true}}
+}]
+express.use(cors())
+var config = {
+  routes: Routes,
+  adapter: require('seneca-web-adapter-express'),
+  context: express,
+  options: {parseBody: true}
+}
+seneca.client()
+.use(web, config)
+.ready(() => {
+  var server = seneca.export('web/context')()
+  server.listen('3000', () => {
+    console.log('server momero started on: 3000')
+  })
+})
+seneca.add({area: 'product', action: 'list'}, function (args, done) {
+  try {
+    done(null, {bar: 'Bazinga!'})
+  } catch (err) {
+    done(err, null)
+  }
+})
